c3f06d0cd2e506b4432f9b6ef7b6e67e9db14d20
godzilla-reject <103082145+godzilla-reject@users.noreply.github.com>
2023-02-11 00:14:51 -0600

    Add files via upload

--- packs/scripts/module.js
+++ packs/scripts/module.js
@@ -1,7 +1,7 @@
 Hooks.on('renderNPCSheetPF2e', async (_, html) => {
     // we look for the tab nav and inject a new tab entry
     const nav = html.find('.npc-body .sheet-tabs.tabs[data-group=primary]')
-    nav.append('<a class="item " data-tab="custom">Custom</a>')
+    nav.append('<a class="item " data-tab="custom">Parts</a>')
 
     // we look for the tabs and inject our rendered template in it
     const template = await renderTemplate('modules/monster-hunter-hack/templates/template.html')
